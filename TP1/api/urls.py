859baeb89049ffda30ba13a62cf4a9297803eee6
Pablo Gallo <gallopabloj@gmail.com>
2026-03-30 09:43:15 -0300

    TP1, ahora si funciona

--- TP1/api/urls.py
+++ TP1/api/urls.py
@@ -3,6 +3,6 @@ from .views import obtener_agregar_items, eliminar_item, modificar_item
 
 urlpatterns = [
     path('items/', obtener_agregar_items, name='obtener_agregar_items'),
-    path('items/<int:id>/', eliminar_item, name= 'eliminar_item'),
-    path('items/ediatr/<int:id>', modificar_item, name= 'modificar_item'),
+    path('items/eliminar/<int:id>/', eliminar_item, name= 'eliminar_item'),
+    path('items/editar/<int:id>', modificar_item, name= 'modificar_item'),
 ]
--- TP1/api/views.py
+++ TP1/api/views.py
@@ -19,7 +19,7 @@ def obtener_agregar_items(request):
 
     
         
-
+@csrf_exempt
 def eliminar_item(request, id):
     if request.method == 'DELETE':
         for item in items:
@@ -27,15 +27,49 @@ def eliminar_item(request, id):
                 items.remove(item)
                 return JsonResponse({'mensaje': 'Item eliminado'}, status = 204)
         return JsonResponse({'mensaje': 'No existe item con ese id'}, status = 400)
-    
+
+@csrf_exempt  
 def modificar_item(request, id):
     if request.method == 'PUT' or request.method == 'PATCH':
         data = json.loads(request.body)
         for item in items:
-            if request.method == 'PATCH':
-                item['nombre'] = data.get('nombre', item['nombre'])
-            elif request.method == 'PUT':
-                item['nombre'] = data.get('nombre', 'sin nombre')
-            return JsonResponse(item, status = 200)
-        return JsonResponse({'errpr': 'No existe item con ese id'}, status = 400)
+            if item['id'] == id:
+                if request.method == 'PATCH':
+                    item['nombre'] = data.get('nombre', item['nombre'])
+                elif request.method == 'PUT':
+                    item['nombre'] = data.get('nombre', 'sin nombre')
+                return JsonResponse(item, status = 200)
+            
+        return JsonResponse({'error': 'No existe item con ese id'}, status = 400)
 
+
+
+#   EDITAR
+# (.venv) PS D:\Documentos\universidad\desarrolloAPIs\trabajos_catedra> curl -X PUT "http://127.0.0.1:8000/api/items/editar/1" -H "Content-Type: application/json" -d
+#  '{"nombre": "Item Actualizado"}'
+# {"id": 1, "nombre": "Item Actualizado"}
+
+
+#   ELIMINAR
+# (.venv) PS D:\Documentos\universidad\desarrolloAPIs\trabajos_catedra> curl -v -X DELETE "http://127.0.0.1:8000/api/items/eliminar/1/"
+# *   Trying 127.0.0.1:8000...
+# * Established connection to 127.0.0.1 (127.0.0.1 port 8000) from 127.0.0.1 port 64211
+# * using HTTP/1.x
+# > DELETE /api/items/eliminar/1/ HTTP/1.1
+# > Host: 127.0.0.1:8000
+# > User-Agent: curl/8.18.0
+# > Accept: */*
+# >
+# * Request completely sent off
+# < HTTP/1.1 204 No Content
+# < Date: Mon, 30 Mar 2026 12:41:45 GMT
+# < Server: WSGIServer/0.2 CPython/3.13.1
+# < Content-Type: application/json
+# < X-Frame-Options: DENY
+# < Content-Length: 29
+# < X-Content-Type-Options: nosniff
+# < Referrer-Policy: same-origin
+# < Cross-Origin-Opener-Policy: same-origin
+# <
+# * Excess found writing body: excess = 29, size = 0, maxdownload = 0, bytecount = 0
+# * shutting down connection #0
